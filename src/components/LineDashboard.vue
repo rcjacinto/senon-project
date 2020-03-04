cf8606031a6409d8d7d063d6b6e280ec4b022b5c
RhandallTimtiman <rhandaaalltimtiman@gmail.com>
2020-03-04 20:28:27 +0800

    Initialized Dashboard UI and Chart

---
+++ src/components/LineDashboard.vue
@@ -0,0 +1,39 @@
+<script>
+import { Line } from 'vue-chartjs'
+export default {
+  extends: Line,
+  mounted () {
+    this.renderChart({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [
+        {
+          label: 'Active',
+          fill: false,
+          backgroundColor: '#4bc0c0',
+          borderColor: '#4bc0c0',
+          data: [1, 2, 3, 4, 7, 0, 1, 2]
+        },
+        {
+          label: 'Inactive',
+          fill: false,
+          backgroundColor: '#c9cbcf',
+          borderColor: '#c9cbcf',
+          data: [2, 5, 1, 6, 3, 2, 5]
+        },
+        {
+          label: 'Due',
+          fill: false,
+          backgroundColor: '#ff6384',
+          borderColor: '#ff6384',
+          data: [9, 2, 1, 3]
+        }
+      ]
+    },
+    {
+      responsive: true,
+      maintainAspectRatio: false
+    })
+  },
+  name: 'BarGraph'
+}
+</script>
--- src/pages/cms/dashboard.vue
+++ src/pages/cms/dashboard.vue
@@ -1,54 +1,96 @@
 <template>
   <q-page padding>
-     <div class="row items-start">
-       <div class="col q-pa-md">
+    <div class="row items-start">
+      <div class="col q-pa-md">
         <q-card class="my-card">
-          <q-card-section class="bg-secondary text-white">
+          <q-card-section class="bg-primary text-white">
 
             <q-icon name="all_inbox" style="font-size:48px;"/>
             <div class="text-h6">Reports</div>
           </q-card-section>
 
         </q-card>
-       </div>
+      </div>
 
-       <div class="col q-pa-md">
+      <div class="col q-pa-md">
         <q-card class="my-card">
-          <q-card-section class="bg-secondary text-white">
+          <q-card-section class="bg-primary text-white">
             <q-icon name="account_box" style="font-size:48px;"/>
             <div class="text-h6">Accounts </div>
           </q-card-section>
 
         </q-card>
-       </div>
+      </div>
 
-       <div class="col q-pa-md">
+      <div class="col q-pa-md">
         <q-card class="my-card">
-          <q-card-section class="bg-secondary text-white">
+          <q-card-section class="bg-primary text-white">
             <q-icon name="assignment" style="font-size:48px;"/>
             <div class="text-h6">Assignments</div>
           </q-card-section>
         </q-card>
-       </div>
-  </div>
+      </div>
+    </div>
+    <div class="row items-start">
+      <div class="col-sm-12 col-md-7 col-lg-7 q-pa-md">
+        <q-card class="my-card">
+          <q-card-section class="text-white">
+
+            <!-- <q-icon name="all_inbox" style="font-size:48px;"/> -->
+            <div><LineDashboard /></div>
+          </q-card-section>
+
+        </q-card>
+      </div>
+      <div class="col-sm-12 col-md-5 col-lg-5 q-pa-md">
+        <q-card class="my-card">
+        <q-card-section>
+          <div class="text-h6 q-mb-xs">Announcement</div>
+        </q-card-section>
+          <q-list>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon color="red" name="notification_important" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>Announcement 1</q-item-label>
+                <q-item-label caption>Have a drink.</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon color="red" name="notification_important" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>Announcement 2</q-item-label>
+                <q-item-label caption>Fill your gas tank.</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon color="red" name="notification_important" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>Announcement 3</q-item-label>
+                <q-item-label caption>Watch a movie.</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
-
 <script>
-import { Bar } from 'vue-chartjs'
+import LineDashboard from '../../components/LineDashboard'
 export default {
-  extends: Bar,
-  mounted () {
-    this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      datasets: [
-        {
-          label: 'GitHub Commits',
-          backgroundColor: '#f87979',
-          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-        }
-      ]
-    })
+  components: {
+    LineDashboard
   }
   // name: 'PageName',
 }
