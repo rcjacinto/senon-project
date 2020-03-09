d0310f8002757737ee679ddc5e511566b861588f
RhandallTimtiman <rhandaaalltimtiman@gmail.com>
2020-03-09 17:22:52 +0800

    Added Barchart to accomodate Reports in Dashboard

---
+++ src/components/BarDashboard.vue
@@ -0,0 +1,85 @@
+<script>
+import { Bar } from 'vue-chartjs'
+export default {
+  extends: Bar,
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.renderChart({
+        labels: this.data.label,
+        datasets: [
+          {
+            label: 'Active',
+            backgroundColor: 'lightblue',
+            borderColor: 'blue',
+            borderWidth: 1,
+            data: this.data && this.data.dataset[0].data
+          },
+          {
+            label: 'Completed',
+            backgroundColor: 'lightgreen',
+            borderColor: 'green',
+            borderWidth: 1,
+            data: this.data && this.data.dataset[1].data
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      })
+    }, 500)
+  },
+  watch: {
+    data () {
+      this.renderChart({
+        labels: this.data.label,
+        datasets: [
+          {
+            label: 'Active',
+            backgroundColor: 'lightblue',
+            borderColor: 'blue',
+            borderWidth: 1,
+            data: this.data && this.data.dataset[0].data
+          },
+          {
+            label: 'Completed',
+            backgroundColor: 'lightgreen',
+            borderColor: 'green',
+            borderWidth: 1,
+            data: this.data && this.data.dataset[1].data
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      })
+    }
+  },
+  name: 'BarGraph'
+}
+</script>
