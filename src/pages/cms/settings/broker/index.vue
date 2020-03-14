7f81d4f97d46d77b6913f98ba18706c83b061280
Rhandaaall <rhandaaalltimtiman@gmail.com>
2020-03-14 23:59:58 +0800

    Added Crud of Insurer, Broker and Policy[WIP]

--- src/pages/cms/settings/broker/index.vue
+++ src/pages/cms/settings/broker/index.vue
@@ -1,13 +1,29 @@
 <template>
   <q-page padding>
     <div class="q-pa-md q-gutter-sm">
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6"> {{ labelText }} Broker</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input label="Broker" :disable="actionType === 'view'" v-model="broker" stack-label :rules="[ val => val && val.length > 0 || 'Required']"/>
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat :label="(actionType === 'view') ? 'Close' : 'Cancel'" v-close-popup />
+          <q-btn flat @click="performAction" v-if="!(actionType === 'view')" :label="buttonText" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
       <!-- <q-breadcrumbs class="text-right">
         <q-breadcrumbs-el label="Insurance Assignment" icon="assignment" to="/cms/assignment" />
         <q-breadcrumbs-el label="Listing" icon="list" />
       </q-breadcrumbs> -->
     </div>
     <div class="q-pa-md">
-      <q-btn color="grey-8" icon="add" label="New Broker"/>
+      <q-btn color="grey-8" @click="openDialog('add', { id: null, broker: '' })" icon="add" label="New Broker"/>
     </div>
     <div class="q-pa-md">
       <q-table
@@ -19,7 +35,11 @@
         <q-tr slot="body" slot-scope="props" :props="props">
           <q-td class="text-green-9">{{props.row.id}}</q-td>
           <q-td>{{props.row.broker}}</q-td>
-          <q-td><q-btn color="grey-8" label="View" v-bind:to="'/cms/settings/broker#'+props.row.id" size="sm" icon="search"/></q-td>
+          <q-td>
+            <q-btn @click="openDialog('view', props.row)" color="grey-8" label="View" size="sm" icon="search"/>
+            &nbsp;
+            <q-btn @click="openDialog('update', props.row)" color="yellow-8" label="Update" size="sm" icon="update"/>
+          </q-td>
         </q-tr>
       </q-table>
     </div>
@@ -30,6 +50,10 @@
 export default {
   data () {
     return {
+      prompt: false,
+      broker: '',
+      id: '',
+      actionType: 'view',
       tableData: '',
       pagination: {
         sortBy: 'desc',
@@ -45,7 +69,56 @@ export default {
       ]
     }
   },
+  computed: {
+    buttonText () {
+      if (this.actionType === 'view') {
+        return 'Confirm'
+      } else if (this.actionType === 'update') {
+        return 'Update'
+      }
+      return 'Add'
+    },
+    labelText () {
+      if (this.actionType === 'view') {
+        return 'View'
+      } else if (this.actionType === 'update') {
+        return 'Update'
+      }
+      return 'Add'
+    }
+  },
   methods: {
+    openDialog (value, data) {
+      this.actionType = value
+      this.prompt = true
+      this.broker = data.broker
+      this.id = data.id
+    },
+    performAction () {
+      this.$q.loading.show()
+      let action = ''
+      if (this.actionType === 'update') {
+        action = this.$axios.patch(`/api/brokers/${this.id}`, { data: { broker: this.broker } })
+      } else {
+        action = this.$axios.post('/api/brokers', { broker: this.broker })
+      }
+      action.then(res => {
+        this.$q.loading.hide()
+        this.$q.notify({
+          color: 'positive',
+          message: `Record has been ${(this.actionType === 'update') ? 'updated' : 'created'} successfully`,
+          position: 'top-right'
+        })
+        this.getListingData()
+      }).catch((err) => {
+        this.$q.notify({
+          color: 'negative',
+          message: err.response.data.message,
+          position: 'top-right'
+        })
+        this.$q.loading.hide()
+      })
+    },
     getListingData () {
       this.$axios.get('api/brokers').then(res => {
         let result = []
--- src/pages/cms/settings/policy/index.vue
+++ src/pages/cms/settings/policy/index.vue
@@ -1,13 +1,29 @@
 <template>
   <q-page padding>
     <div class="q-pa-md q-gutter-sm">
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6"> {{ labelText }} Policy</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input label="Policy" :disable="actionType === 'view'" v-model="policy" stack-label :rules="[ val => val && val.length > 0 || 'Required']"/>
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat :label="(actionType === 'view') ? 'Close' : 'Cancel'" v-close-popup />
+          <q-btn flat @click="performAction" v-if="!(actionType === 'view')" :label="buttonText" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
       <!-- <q-breadcrumbs class="text-right">
         <q-breadcrumbs-el label="Insurance Assignment" icon="assignment" to="/cms/assignment" />
         <q-breadcrumbs-el label="Listing" icon="list" />
       </q-breadcrumbs> -->
     </div>
     <div class="q-pa-md">
-      <q-btn color="grey-8" icon="add" label="New Policy"/>
+      <q-btn @click="openDialog('add', { id: null, policy: '' })" color="grey-8" icon="add" label="New Policy"/>
     </div>
     <div class="q-pa-md">
       <q-table
@@ -19,7 +35,11 @@
         <q-tr slot="body" slot-scope="props" :props="props">
           <q-td class="text-green-9">{{props.row.id}}</q-td>
           <q-td>{{props.row.policy_type}}</q-td>
-          <q-td><q-btn color="grey-8" label="View" v-bind:to="'/cms/settings/policy#'+props.row.id" size="sm" icon="search"/></q-td>
+          <q-td>
+            <q-btn @click="openDialog('view', props.row)" color="grey-8" label="View" size="sm" icon="search"/>
+            &nbsp;
+            <q-btn @click="openDialog('update', props.row)" color="yellow-8" label="Update" size="sm" icon="update"/>
+          </q-td>
         </q-tr>
       </q-table>
     </div>
@@ -30,6 +50,10 @@
 export default {
   data () {
     return {
+      prompt: false,
+      policy: '',
+      id: '',
+      actionType: 'view',
       tableData: '',
       pagination: {
         sortBy: 'desc',
@@ -45,7 +69,56 @@ export default {
       ]
     }
   },
+  computed: {
+    buttonText () {
+      if (this.actionType === 'view') {
+        return 'Confirm'
+      } else if (this.actionType === 'update') {
+        return 'Update'
+      }
+      return 'Add'
+    },
+    labelText () {
+      if (this.actionType === 'view') {
+        return 'View'
+      } else if (this.actionType === 'update') {
+        return 'Update'
+      }
+      return 'Add'
+    }
+  },
   methods: {
+    openDialog (value, data) {
+      this.actionType = value
+      this.prompt = true
+      this.policy = data.policy_type
+      this.id = data.id
+    },
+    performAction () {
+      this.$q.loading.show()
+      let action = ''
+      if (this.actionType === 'update') {
+        action = this.$axios.patch(`/api/policy/${this.id}`, { data: { policy: this.policy } })
+      } else {
+        action = this.$axios.post('/api/policy', { policy: this.policy })
+      }
+      action.then(res => {
+        this.$q.loading.hide()
+        this.$q.notify({
+          color: 'positive',
+          message: `Record has been ${(this.actionType === 'update') ? 'updated' : 'created'} successfully`,
+          position: 'top-right'
+        })
+        this.getListingData()
+      }).catch((err) => {
+        this.$q.notify({
+          color: 'negative',
+          message: err.response.data.message,
+          position: 'top-right'
+        })
+        this.$q.loading.hide()
+      })
+    },
     getListingData () {
       this.$axios.get('api/policy').then(res => {
         let result = []
