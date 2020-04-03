8bba5ee92510ab7dd952e144cbb3659b9dc787b6
Rhandaaall <rhandaaalltimtiman@gmail.com>
2020-04-03 11:20:35 +0800

    Fixed Log out Error after Login

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,13 +3,21 @@ import axios from 'axios'
 
 Vue.prototype.$axios = axios
 export default ({ Vue }) => {
-  const loginInfo = localStorage.loginInfo ? JSON.parse(localStorage.loginInfo) : null
-  if (loginInfo) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${loginInfo.token}`
-  }
   axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8;application/x-www-form-urlencoded'
   axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
   axios.defaults.baseURL = 'http://18.162.168.111/'
   // axios.defaults.baseURL = 'http://ec2-18-136-207-167.ap-southeast-1.compute.amazonaws.com/sureloan-ph'
+  axios.interceptors.request.use(
+    (config) => {
+      const loginInfo = localStorage.loginInfo ? JSON.parse(localStorage.loginInfo) : null
+      if (loginInfo) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${loginInfo.token}`
+      }
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
   Vue.prototype.$axios = axios
 }
