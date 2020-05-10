e5fa38e98a584147b98af0d391f4e5958b94fba3
rcjacinto <robbiecarlo.jacinto@gmail.com>
2020-05-11 01:33:26 +0800

    firestore

---
+++ src/store/fire-store.js
@@ -0,0 +1,20 @@
+const state = {
+
+}
+const mutations = {
+
+}
+const actions = {
+
+}
+const getters = {
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import example from './module-example'
+import fireStore from './fire-store'
 
 Vue.use(Vuex)
 
@@ -17,7 +17,7 @@ Vue.use(Vuex)
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      fireStore
     },
 
     // enable strict mode (adds overhead!)
